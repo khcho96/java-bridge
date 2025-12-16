eb93808f8884f90aa7f85c754e36a42acc4fea58
khcho96 <khcho1492@gmail.com>
2025-12-16 19:52:28 +0900

    fix: 출력문 포맷팅 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
     private static final String START = "다리 건너기 게임을 시작합니다.\n";
     private static final String GAME_RESULT = "최종 게임 결과";
     private static final String FINAL_RESULT = "게임 성공 여부: ";
-    private static final String TRY_COUNT = "총 시도한 횟수 :";
+    private static final String TRY_COUNT = "총 시도한 횟수: ";
 
     public static void printStart() {
         System.out.println(START);
